2fb9f90267377aa3a9058e6e86de8ec4a971d425
Daigo <daigo.python.info@gmail.com>
2024-09-20 16:14:26 +0900

    mod

--- app/Books/[bookId]/page.tsx
+++ app/Books/[bookId]/page.tsx
@@ -1,6 +1,6 @@
 import "../scss/Books.scss";
 
-interface bookProp {
+interface BookProp {
   id: number;
   title: {
     rendered: string;
@@ -10,28 +10,24 @@ interface bookProp {
   };
 }
 
-
-// generateStaticParams を追加
+// 静的に生成する本の ID リストを取得
 export async function generateStaticParams() {
-  // 本の ID リストを取得するために API を呼び出します
-  const books = await fetch(`http://mneko0904.cloudfree.jp/owner/books/wp-json/wp/v2/posts`);
-  const booksData = await books.json();
+  const response = await fetch(`http://mneko0904.cloudfree.jp/owner/books/wp-json/wp/v2/posts`);
+  
+  if (!response.ok) {
+    throw new Error('Failed to fetch books');
+  }
 
-  // 静的に生成する本の ID リストを返す
-  return booksData.map(( book:bookProp ) => ({
+  const booksData: BookProp[] = await response.json();
+
+  return booksData.map((book) => ({
     bookId: String(book.id),
   }));
 }
 
-
-
-export default async function BookPage({
-  params,
-}: {
-  params: { bookId: string };
-}) {
-
+export default async function BookPage({ params }: { params: { bookId: string } }) {
   const { bookId } = params;
+
   // bookIdを使って本の詳細を取得
   const response = await fetch(`http://mneko0904.cloudfree.jp/owner/books/wp-json/wp/v2/posts/${bookId}`);
 
@@ -39,15 +35,13 @@ export default async function BookPage({
     throw new Error('Failed to fetch book details');
   }
 
-  const bookData:BookProp = await response.json();
+  const bookData: BookProp = await response.json();
 
   return (
     <div className="book_page">
-
       <article className="book_inner">
         <div className="inner_post">
           <h2>{bookData.title.rendered}</h2>
-
           <div
             className="post_content"
             dangerouslySetInnerHTML={{ __html: bookData.content.rendered }}
@@ -57,6 +51,3 @@ export default async function BookPage({
     </div>
   );
 }
-
-
-
