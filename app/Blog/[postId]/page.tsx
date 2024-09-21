5a515a755412d20a225b0e17d953e031ec27b604
Daigo <daigo.python.info@gmail.com>
2024-09-21 09:34:42 +0900

    mod

--- app/Blog/[postId]/page.tsx
+++ app/Blog/[postId]/page.tsx
@@ -3,35 +3,35 @@ import Not_Image from "../img/image.png";
 import "./scss/post.scss";
 
 
+const RestAPI = "https://hikaricreative.fool.jp/wp-json/wp/v2/posts";
+
 // 静的生成
 export async function generateStaticParams() {
   return []
 }
 
+export default async function PostPage({ params }: { params: { postId: string } }) {
+  const { postId } = params;
 
-export default async function PostPage({
-  params,
-}: {
-  params: { postId: string };
-}) {
-  const data = await fetch(
-    `https://hikaricreative.fool.jp/wp-json/wp/v2/posts/${params.postId}`,
-  );
-  const post = await data.json();
+  const post_res = await fetch(`${RestAPI}/${postId}`,{
+    method: 'GET',
+    cache: "no-cache",
+  }).then((res) => res.json());
+
 
   return (
     <>
       <p>postIdページになります。ID: {params.postId}</p>
 
       <article className="post_page">
-        <div className="inner_post" key={post.id}>
+        <div className="inner_post" key={post_res.id}>
           <div className="eye_top">
             <Image className="eye_catch" src={Not_Image} alt="画像なし" />
-            <h2>{post.title.rendered}</h2>
+            <h2>{post_res.title.rendered}</h2>
           </div>
           <div
             className="post_content"
-            dangerouslySetInnerHTML={{ __html: post.content.rendered }}
+            dangerouslySetInnerHTML={{ __html: post_res.content.rendered }}
           />
         </div>
       </article>
