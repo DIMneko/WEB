f93a817a6ec8678ee86cd43dd7f39370be251071
Daigo <daigo.python.info@gmail.com>
2024-09-24 14:22:40 +0900

    Books

--- app/Books/page.tsx
+++ app/Books/page.tsx
@@ -1,7 +1,16 @@
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
 
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+
+// import Image from "next/image";
+import Typography from "@mui/material/Typography";
+import Link from "next/link";
 
 import "@/app/Blog/scss/blog.scss";
-import Link from "next/link";
+import Not_Image from "@/app/Blog/img/image.png";
+
 
 interface PostProp {
   id: number;
@@ -18,7 +27,9 @@ interface PostProp {
 
 async function getPosts() {
   const ENDPOINT = "http://mneko0904.cloudfree.jp/owner/books/wp-json/wp/v2/posts"
-  const res = await fetch(ENDPOINT)
+  const res = await fetch(ENDPOINT,{
+    cache: "no-store",
+  })
   const data = await res.json()
   return data
 }
@@ -28,12 +39,30 @@ export default async function Blog_page() {
   const posts = await getPosts()
   return (
     <>
-    {posts.map((post:PostProp)=>(
-      <div key={post.id}>
-        <h3>{post.title.rendered}</h3>
-        <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/Blog/${post.id}`}>もっと見る</Link>
-      </div>
-    ))}
+      <ul className="news_area">
+        {posts.map((post: PostProp) => (
+          <Card className="post_archive" key={post.id}>
+            <CardActionArea>
+              <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/Blog/${post.id}`}>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={Not_Image.src}
+                  alt="NotImage"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {post.title.rendered}
+                  </Typography>
+                  <div
+                    dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+                  />
+                </CardContent>
+              </Link>
+            </CardActionArea>
+          </Card>
+        ))}
+      </ul>
     </>
   );
 }
