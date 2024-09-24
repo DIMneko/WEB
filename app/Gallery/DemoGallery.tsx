153b53369a6d07b5372839c73b6e76bbf919f4f2
Daigo <daigo.python.info@gmail.com>
2024-09-24 17:26:40 +0900

    gal

--- app/Gallery/DemoGallery.tsx
+++ app/Gallery/DemoGallery.tsx
@@ -12,76 +12,156 @@ import G_item011 from "./img/g_items011.png";
 import G_item012 from "./img/g_items012.png";
 import G_item013 from "./img/g_items013.png";
 import G_item014 from "./img/g_items014.png";
+import G_item015 from "./img/g_items015.png";
+import G_item016 from "./img/g_items016.png";
+import G_item017 from "./img/g_items017.png";
+import G_item018 from "./img/g_items018.png";
+import G_item019 from "./img/g_items019.png";
+import G_item020 from "./img/g_items020.png";
+import G_item021 from "./img/g_items021.png";
+import G_item022 from "./img/g_items022.png";
+import G_item023 from "./img/g_items023.png";
+import G_item024 from "./img/g_items024.png";
+import G_item025 from "./img/g_items025.png";
+import G_item026 from "./img/g_items026.png";
+import G_item027 from "./img/g_items027.png";
+
 
 export const DemoGallerys = [
   {
     id: 1,
     label: "ラベル名",
     URL: G_item001.src,
-  },
-  {
+},
+{
     id: 2,
     label: "ラベル名",
     URL: G_item002.src,
-  },
-  {
+},
+{
     id: 3,
     label: "ラベル名",
     URL: G_item003.src,
-  },
-  {
+},
+{
     id: 4,
     label: "ラベル名",
     URL: G_item004.src,
-  },
-  {
+},
+{
     id: 5,
     label: "ラベル名",
     URL: G_item005.src,
-  },
-  {
+},
+{
     id: 6,
     label: "ラベル名",
     URL: G_item006.src,
-  },
-  {
+},
+{
     id: 7,
     label: "ラベル名",
     URL: G_item007.src,
-  },
-  {
+},
+{
     id: 8,
     label: "ラベル名",
     URL: G_item008.src,
-  },
-  {
+},
+{
     id: 9,
     label: "ラベル名",
     URL: G_item009.src,
-  },
-  {
+},
+{
     id: 10,
     label: "ラベル名",
     URL: G_item010.src,
-  },
-  {
+},
+{
     id: 11,
     label: "ラベル名",
     URL: G_item011.src,
-  },
-  {
+},
+{
     id: 12,
     label: "ラベル名",
     URL: G_item012.src,
-  },
-  {
+},
+{
     id: 13,
     label: "ラベル名",
     URL: G_item013.src,
-  },
-  {
+},
+{
     id: 14,
     label: "ラベル名",
     URL: G_item014.src,
-  },
+},
+{
+    id: 15,
+    label: "ラベル名",
+    URL: G_item015.src,
+},
+{
+    id: 16,
+    label: "ラベル名",
+    URL: G_item016.src,
+},
+{
+    id: 17,
+    label: "ラベル名",
+    URL: G_item017.src,
+},
+{
+    id: 18,
+    label: "ラベル名",
+    URL: G_item018.src,
+},
+{
+    id: 19,
+    label: "ラベル名",
+    URL: G_item019.src,
+},
+{
+    id: 20,
+    label: "ラベル名",
+    URL: G_item020.src,
+},
+{
+    id: 21,
+    label: "ラベル名",
+    URL: G_item021.src,
+},
+{
+    id: 22,
+    label: "ラベル名",
+    URL: G_item022.src,
+},
+{
+    id: 23,
+    label: "ラベル名",
+    URL: G_item023.src,
+},
+{
+    id: 24,
+    label: "ラベル名",
+    URL: G_item024.src,
+},
+{
+    id: 25,
+    label: "ラベル名",
+    URL: G_item025.src,
+},
+{
+    id: 26,
+    label: "ラベル名",
+    URL: G_item026.src,
+},
+{
+    id: 27,
+    label: "ラベル名",
+    URL: G_item027.src,
+}
+
 ];
