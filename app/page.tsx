c2d8d9ee320376fda0599effd99c94b0877e8604
Daigo <daigo.python.info@gmail.com>
2024-09-26 14:42:55 +0900

    home

--- app/page.tsx
+++ app/page.tsx
@@ -22,22 +22,14 @@ export default async function Home() {
 
   const posts = await getPosts();
 
-  // 日付フォーマット関数を外部で定義
-  const formatDate = (isoDateString: string): string => {
-    const date = new Date(isoDateString);
-    return date.toLocaleDateString('ja-JP', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    });
-  };
+
 
   return (
     <main className="box-border py-12">
       <h2 className="py-8 mx-auto w-[800px] text-center">HOME部分の内容は、ログイン後のダッシュボード画面から追加できるようにしたい。</h2>
       {posts.map((post: PostProp) => (
         <div key={post.id} className="w-[600px] mx-auto flex gap-4 py-2">
-          <p>{formatDate(post.createddata)}</p>
+          <p>{post.createddata}</p>
           <p>{post.title}</p>
         </div>
       ))}
