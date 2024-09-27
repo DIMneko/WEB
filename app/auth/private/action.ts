85725a980c641a29cf564ef76b8d60092b0b94ff
Daigo <daigo.python.info@gmail.com>
2024-09-27 12:28:17 +0900

    try

--- app/auth/private/action.ts
+++ app/auth/private/action.ts
@@ -4,8 +4,8 @@
 import { redirect } from "next/navigation"
 import { createClient } from "@/utils/supabase/server"
 
-
 // ログアウト
+
 export async function LogOut() {
     const supabase = createClient()
     const { error } = await supabase.auth.signOut();
